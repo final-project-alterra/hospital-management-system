d7da74880628359c5875f7a2c211cc664ee78306
alfi2811 <alfinasution64@gmail.com>
2021-12-22 10:26:24 +0700

    add card in dashboard

--- src/pages/admin/dashboard/index.jsx
+++ src/pages/admin/dashboard/index.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import OrganismsDashboardCardGroup from '../../../components/organisms/dashboard/cardGroup'
+import { useHistory } from 'react-router-dom';
+import OrganismsAdminDashboardHighlight from '../../../components/organisms/admin/dashboard/highlight'
+import OrganismsAdminDashboardCardGroup from '../../../components/organisms/admin/dashboard/cardGroup'
 import LayoutsCms from '../../../layouts/cms'
 import { put_data_admin } from '../../../redux/actions/admin'
 import './style.scss'
 
 const AdminDashboard = () => {
+  const history = useHistory();  
   const breadcrumb = [
     {
       label: 'Admin',
@@ -37,7 +40,27 @@ const AdminDashboard = () => {
       title: "Total Outpatient",
       total: "22.000"
     },
-  ]
+  ];
+  const card_data = [
+    {
+      title: "Doctor Data",
+      desc: "See Doctor’s Work Schedule to know date and time schedule in Hospital",
+      url: '/admin/data/doctor',
+      img_url: '',
+    },
+    {
+      title: "Patient Data",
+      desc: "See Doctor’s Work Schedule to know date and time schedule in Hospital",
+      url: '/admin/data/patient',
+      img_url: '',
+    },
+    {
+      title: "Patient Data",
+      desc: "See Doctor’s Work Schedule to know date and time schedule in Hospital",
+      url: '/data',
+      img_url: '',
+    },
+  ];
   const dispatch = useDispatch()  
   useEffect(() => {
     dispatch(put_data_admin("highlight_data", highlight_data))
@@ -45,10 +68,17 @@ const AdminDashboard = () => {
   }, [])
   const highlights = useSelector(state => state.admin?.highlight_data)  
   console.log(highlights)
+  const goToUrl = (url) => {
+    history.push(url);
+  }
   return (
     <LayoutsCms activeMenu={activeMenu} breadcrumb={breadcrumb}>
       <div className='p-admin-dashboard'>        
-        <OrganismsDashboardCardGroup initialHighlightData={highlights} />
+        <OrganismsAdminDashboardHighlight initialHighlightData={highlights} />
+        <OrganismsAdminDashboardCardGroup 
+          initialCardData={card_data} 
+          goToUrl={goToUrl}
+        />
       </div>
     </LayoutsCms>
   )

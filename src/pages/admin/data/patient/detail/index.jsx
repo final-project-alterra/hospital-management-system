2a259619aedffdd10d3924c6619432adef869da2
alfi2811 <alfinasution64@gmail.com>
2022-01-03 11:47:50 +0700

    integration in admin-data

--- src/pages/admin/data/patient/detail/index.jsx
+++ src/pages/admin/data/patient/detail/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom';
 
 import LayoutsCms from '../../../../../layouts/cms';
 import OrganismsAdminDataPatientDetailHeader from '../../../../../components/organisms/admin/data/patient/detail/header';
 import OrganismsAdminDataPatientDetailHistory from '../../../../../components/organisms/admin/data/patient/detail/history';
 import OrganismsAdminDataPatientDetailProfile from '../../../../../components/organisms/admin/data/patient/detail/profile';
+import { useDispatch, useSelector } from 'react-redux';
+import { get_data } from '../../../../../redux/actions/admin';
 
 const AdminDataPatientDetail = () => {
+  const dispatch = useDispatch();
   const history = useHistory();
   const activeMenu = {
     key: 'data-patient',
@@ -30,6 +33,14 @@ const AdminDataPatientDetail = () => {
       url: '/admin/data/patient/detail',
     },
   ];
+  let { id } = useParams();
+  console.log("id: ", id)
+  useEffect(() => {
+    dispatch(get_data(`patients/${id}`, 'patient_data'));
+  }, [dispatch, id]);
+  const initialPatientData = useSelector(state => state.admin?.patient_data)
+  console.log(initialPatientData)
+
   const goBack = () => {
     history.push('/admin/data/patient');
   }
@@ -38,7 +49,7 @@ const AdminDataPatientDetail = () => {
     <LayoutsCms activeMenu={activeMenu} breadcrumb={breadcrumb}>
       <div className="p-admin-data-patient-detail">
         <OrganismsAdminDataPatientDetailHeader goBack={goBack} />
-        <OrganismsAdminDataPatientDetailProfile />
+        <OrganismsAdminDataPatientDetailProfile data={initialPatientData} />
         <OrganismsAdminDataPatientDetailHistory />
       </div>
     </LayoutsCms>
